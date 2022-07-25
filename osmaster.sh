27e3c94c22eb62da7d8aeb91d9e6a1aaae0a5b63
matteocucchi <matteo.cucchi98@gmail.com>
2022-07-25 12:13:43 +0200

    update

--- osmaster.sh
+++ osmaster.sh
@@ -38,6 +38,9 @@ sudo sysctl -p
 
 
 echo "[task 3] download oc"
+
+sudo yum install -y wget
+
 wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 
 tar xvf openshift-origin-client-tools*.tar.gz
